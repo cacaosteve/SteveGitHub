6d08473814ac9cbfd27296081a330ba42f53ad74
Stephen Zyszkiewicz <stevez@me.com>
2021-01-15 17:31:04 -0600

    initial commit

---
+++ SteveGitHub/SteveGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteveGitHubApp.swift
+//  SteveGitHub
+//
+//  Created by steve on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SteveGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
