8d30bce86a8e90153b9df27756835f55a1006514
Stephen Zyszkiewicz <stevez@me.com>
2021-01-21 20:57:17 -0600

    test the navigation bar UI

--- SteveGitHubUITests/SteveGitHubUITests.swift
+++ SteveGitHubUITests/SteveGitHubUITests.swift
@@ -22,15 +22,20 @@ class SteveGitHubUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testTableIsEmpty() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
         
         XCTAssertEqual(app.tables.cells.count, 0)
+    }
+    
+    func testNavigationBarTitle() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.navigationBars["Commits"].staticTexts["Commits"].exists)
     }
 
     func testLaunchPerformance() throws {
